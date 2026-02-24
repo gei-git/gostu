42a840f581310b9bade01fe07d538db897e9c7fd
gei-git <geiformail@163.com>
2026-02-24 13:11:32 +0800

    add

--- cmd/add.go
+++ cmd/add.go
@@ -11,9 +11,12 @@ import (
 
 // addCmd represents the add command
 var addCmd = &cobra.Command{
-	Use:   "add[任务内容]",
+	Use:   "add [任务内容]",
 	Short: "添加一个新任务",
-	Long:  `添加任务，支持优先级和截止日期，例如：todo add "写周报" --priority high --due "2026-03-01"`,
+	Long: `添加一个新任务，支持优先级、截止日期、标签。
+示例：
+  todo add "写周报" --priority=high --due=2025-03-01
+  todo add "健身" --priority=low`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("add called")
 	},
