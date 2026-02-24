7440207c2c713d37146702173b3d80bd392f5874
gei-git <geiformail@163.com>
2026-02-24 20:21:38 +0800

    写完 create 和 list

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module github.com/gei-git/todo-cli
+
 go 1.24.4
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	go.etcd.io/bbolt v1.4.3 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 )
---
+++ internal/storage/bolt.go
@@ -0,0 +1,82 @@
+package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/gei-git/todo-cli/internal/model" // ← 注意：如果你的 go.mod module 名不是 gostu，请改成你的实际模块名！
+
+	"go.etcd.io/bbolt"
+)
+
+const (
+	bucketName = "tasks"
+	dbFile     = "tasks.db"
+)
+
+// TaskStore BoltDB 任务存储实现（企业级单例模式）
+type TaskStore struct {
+	db *bbolt.DB
+}
+
+// NewTaskStore 创建或打开 BoltDB 文件
+func NewTaskStore() (*TaskStore, error) {
+	db, err := bbolt.Open(dbFile, 0600, &bbolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return nil, fmt.Errorf("打开数据库失败: %w", err)
+	}
+
+	// 创建 bucket（如果不存在）
+	err = db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		return err
+	})
+	if err != nil {
+		db.Close()
+		return nil, fmt.Errorf("创建 bucket 失败: %w", err)
+	}
+
+	return &TaskStore{db: db}, nil
+}
+
+// Close 关闭数据库连接
+func (s *TaskStore) Close() error {
+	return s.db.Close()
+}
+
+// Create 添加新任务
+func (s *TaskStore) Create(task *model.Task) error {
+	return s.db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+
+		// 自动生成 ID（最大 ID + 1）
+		id, _ := b.NextSequence()
+		task.ID = uint(id)
+
+		buf, err := json.Marshal(task)
+		if err != nil {
+			return err
+		}
+
+		return b.Put([]byte(fmt.Sprintf("%d", task.ID)), buf)
+	})
+}
+
+// List 获取所有任务
+func (s *TaskStore) List() ([]*model.Task, error) {
+	var tasks []*model.Task
+
+	err := s.db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		return b.ForEach(func(k, v []byte) error {
+			var t model.Task
+			if err := json.Unmarshal(v, &t); err != nil {
+				return err
+			}
+			tasks = append(tasks, &t)
+			return nil
+		})
+	})
+	return tasks, err
+}
